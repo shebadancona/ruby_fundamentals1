807588fff3f4e7115f57d128becc2246f4626aea
shebadancona <shebadancona@gmail.com>
2018-12-12 22:21:27 -0500

    exersice 1

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,11 @@
 puts 2
 puts 3
 puts 2 + 3
+puts 'betty\'s pie ship'
+puts "ada lovelace lived for #{1852 - 1815} years."
+puts  " hello \a hello"
+puts " hello \" helloe"
+puts "hello\t\tworld"
+puts "hello\b\b\b\b\b\bGoodbye world"
+puts "hellow\rStart over world"
+puts "1. hello\n2. world"
