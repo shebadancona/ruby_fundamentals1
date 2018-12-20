44fb2b5c376c5ce085946d7cb3db91395a020da7
shebadancona <shebadancona@gmail.com>
2018-12-19 22:24:19 -0500

    end of assignment

--- exercise5.rb
+++ exercise5.rb
@@ -1,36 +1,29 @@
 
 
-
+energy = 10
 km = 0
 
 ans = "walk"
 
-until ans == "go home" do 
+until ans == "go home" || energy ==  0 do 
+    
     
     puts "would you like to walk or run"
 ans = gets.chomp
 
 if ans == "walk"
+    energy = energy + 1
     km += 1
     puts "distance from home is #{km}"
 
 elsif ans == "run"
     km += 5
+    energy = energy  -1
     puts "distance from home is #{km}"
+elsif ans == "go home"
+    break
 else
     puts "this command does not exist"
 
 end
 end
-# if  ans == walk
-#     puts " distance from home is #{1}"
-# elsif ans == run
-#     puts "distance from home is #{}
-
-
-#     until ans == "go home"
-#         km = 0
-#         km +=1
-#         puts "distance from home is #{km}km"
-# end
-
